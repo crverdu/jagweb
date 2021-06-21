7f3c996aef43a9b8e8f21160d1e9ac532e4b2d4c
crverdu <cristian.krutki@gmail.com>
2021-06-21 17:58:47 -0300

    models Producto

--- JAGUARETE/JAGWEB/models.py
+++ JAGUARETE/JAGWEB/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Producto (models.Model):
+    nombre = models.CharField(max_length=180)
+    descripcion = models.CharField(max_length=250)
+    categoria = models.CharField(max_length=64)
+    precio=models.FloatField()
+
+    def __str__(self):
+        return f"Product ID #{self.id}: nombre {self.nombre} desc {self.descripcion} categoria {self.categoria} precio $ {self.precio}"
+
