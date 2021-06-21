72f12d82045beb33798ba10497ad3002ec309088
crverdu <cristian.krutki@gmail.com>
2021-06-21 14:38:59 -0300

    entradas product,search,about,home

--- JAGUARETE/JAGWEB/urls.py
+++ JAGUARETE/JAGWEB/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index,name="index"),
+    path('about', views.about, name="about"),
+    path('search',views.searchResoult,name="search"),
+    path('product',views.product,name="product"),
 ]
