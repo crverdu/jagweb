f7f17ad6dfd93fccde27cc7e0e1cba816ed3b35b
crverdu <cristian.krutki@gmail.com>
2021-07-08 11:04:54 -0300

    dirs

--- JAGUARETE/JAGUARETE/settings.py
+++ JAGUARETE/JAGUARETE/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'JAGUARETE.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r'E:\CursoPoloTic\ProyectoFinal\polofinal\JAGUARETE\JAGWEB\templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,10 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 #Media files de JAGWEB
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
